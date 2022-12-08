9656147ffd80df17502f8ca050bafda087dc61cb
ignassew <szpiren@gmail.com>
2022-12-08 19:43:07 +0100

    fix typo

--- src/browser/chrome/ver/mod.rs
+++ src/browser/chrome/ver/mod.rs
@@ -12,6 +12,6 @@ pub(super) fn get_config_from_ver(ver: ChromeVersion) -> BrowserSettings {
         ChromeVersion::V104 => v104::get_settings(),
         ChromeVersion::V105 => v105::get_settings(),
         ChromeVersion::V106 => v106::get_settings(),
-        ChromeVersion::V108 => v106::get_settings(),
+        ChromeVersion::V108 => v108::get_settings(),
     }
 }
