3dfae74bfdc8c002585c3c5541be712b072fb22f
ignassew <szpiren@gmail.com>
2023-02-17 19:10:08 +0100

    Reduce code duplication for chrome settings

--- src/browser/chrome/ver/mod.rs
+++ src/browser/chrome/ver/mod.rs
@@ -6,12 +6,13 @@ mod v104;
 mod v105;
 mod v106;
 mod v108;
+mod v106_110;
 
 pub(super) fn get_config_from_ver(ver: ChromeVersion) -> BrowserSettings {
     match ver {
         ChromeVersion::V104 => v104::get_settings(),
         ChromeVersion::V105 => v105::get_settings(),
-        ChromeVersion::V106 => v106::get_settings(),
-        ChromeVersion::V108 => v108::get_settings(),
+        ChromeVersion::V106 => v106_110::get_settings(v106::create_headers()),
+        ChromeVersion::V108 => v106_110::get_settings(v108::create_headers()),
     }
 }
